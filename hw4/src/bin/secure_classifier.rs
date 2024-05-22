ffb4c4f1bb9b8d0d1fd1af016eb096c326726cc6
dlddu <39251873+dlddu@users.noreply.github.com>
2024-05-22 04:39:15 +0000

    chore: Update devcontainer.json to use Rust 1-1-bullseye image and install rust-analyzer extension

--- hw4/src/bin/secure_classifier.rs
+++ hw4/src/bin/secure_classifier.rs
@@ -1,14 +1,24 @@
-use std::env;
 use darknet::image_classifier;
+use std::{env, fs};
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  let input_filename = &args[1];
-  let filename_cstr = std::ffi::CString::new(input_filename.clone()).unwrap();
+    let args: Vec<String> = env::args().collect();
+    let input_filename = &args[1];
+    let parameters = fs::read_to_string(input_filename)
+        .unwrap()
+        .lines()
+        .map(|line| {
+            line.split(':')
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>()
+        })
+        .collect::<Vec<Vec<String>>>();
 
-  let top_k = 3;
+    for parameter in parameters {
+        let _user = parameter.first().unwrap();
+        let filename = parameter.get(1).unwrap();
+        let top_k = parameter.get(2).unwrap().parse::<i32>().unwrap();
 
-  unsafe {
-    image_classifier(filename_cstr.as_ptr(), top_k);
-  }
+        unsafe { image_classifier(filename.as_ptr(), top_k) };
+    }
 }
