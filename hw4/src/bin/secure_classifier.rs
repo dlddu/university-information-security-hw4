2b4923af39bc4ed1d2b0be94fef65da814ff04a1
dlddu <39251873+dlddu@users.noreply.github.com>
2024-06-02 20:03:04 +0900

    chore: Refactor file handling in image_classifier function

--- hw4/src/bin/secure_classifier.rs
+++ hw4/src/bin/secure_classifier.rs
@@ -1,12 +1,12 @@
 use darknet::image_classifier;
-use libseccomp::{ScmpAction, ScmpArgCompare, ScmpCompareOp, ScmpFilterContext, ScmpSyscall};
-use nix::libc::{O_ACCMODE, O_RDONLY, SIGCHLD};
+use libseccomp::{ScmpAction, ScmpFilterContext, ScmpSyscall};
+use nix::libc::SIGCHLD;
 use nix::mount::{mount, umount2, MntFlags, MsFlags};
 use nix::sched::{clone, CloneFlags};
 use nix::sys::wait::{waitid, WaitPidFlag};
 use nix::unistd::{chdir, pivot_root, Pid};
 use std::ffi::{CStr, CString};
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::{env, fs};
 
 fn main() {
@@ -54,16 +54,24 @@ fn main() {
 }
 
 fn run_image_classifier(username: &String, filename: &String, top_k: i32) {
-    fs::create_dir(Path::new("data").join(username).join("put_old")).unwrap_or_default();
-
-    mount::<PathBuf, str, str, str>(
-        Some(&Path::new("data").join(username)),
-        "/mnt",
-        None,
-        MsFlags::MS_BIND,
-        None,
+    fs::create_dir("mnt").unwrap_or_default();
+    fs::create_dir(Path::new("mnt").join("put_old")).unwrap_or_default();
+    fs::create_dir(Path::new("mnt").join("data")).unwrap_or_default();
+    fs::copy("darknet.cfg", Path::new("mnt").join("darknet.cfg")).unwrap();
+    fs::copy("darknet.weights", Path::new("mnt").join("darknet.weights")).unwrap();
+    fs::copy(
+        "imagenet.shortnames.list",
+        Path::new("mnt").join("imagenet.shortnames.list"),
     )
-    .expect("mount data dir");
+    .unwrap();
+    fs::copy(
+        Path::new("data").join(username).join(filename),
+        Path::new("mnt").join("data").join(filename),
+    )
+    .unwrap();
+
+    mount::<str, str, str, str>(Some("mnt"), "/mnt", None, MsFlags::MS_BIND, None)
+        .expect("mount data dir");
 
     pivot_root(Path::new("/mnt"), &Path::new("/mnt").join("put_old")).expect("swap root");
 
@@ -72,59 +80,26 @@ fn run_image_classifier(username: &String, filename: &String, top_k: i32) {
     umount2(Path::new("/put_old"), MntFlags::MNT_DETACH).expect("confine");
 
     fs::remove_dir("/put_old").unwrap_or_default();
-    fs::remove_file("results.txt").unwrap_or_default();
 
-    let mut context = ScmpFilterContext::new_filter(ScmpAction::Errno(1)).unwrap();
+    let mut context = ScmpFilterContext::new_filter(ScmpAction::Allow).unwrap();
 
     context
-        .add_rule_conditional(
-            ScmpAction::Allow,
-            ScmpSyscall::from_name("write").unwrap(),
-            &[ScmpArgCompare::new(0, ScmpCompareOp::Equal, 1)],
+        .add_rule(
+            ScmpAction::Errno(1),
+            ScmpSyscall::from_name("socket").unwrap(),
         )
-        .unwrap();
-    context
-        .add_rule_conditional(
-            ScmpAction::Allow,
-            ScmpSyscall::from_name("write").unwrap(),
-            &[ScmpArgCompare::new(0, ScmpCompareOp::Equal, 2)],
-        )
-        .unwrap();
-    context
-        .add_rule(ScmpAction::Allow, ScmpSyscall::from_name("getcwd").unwrap())
-        .unwrap();
-    context
-        .add_rule_conditional(
-            ScmpAction::Allow,
-            ScmpSyscall::from_name("open").unwrap(),
-            &[ScmpArgCompare::new(
-                1,
-                ScmpCompareOp::MaskedEqual(O_ACCMODE as u64),
-                O_RDONLY as u64,
-            )],
-        )
-        .unwrap();
-    context
-        .add_rule_conditional(
-            ScmpAction::Allow,
-            ScmpSyscall::from_name("openat").unwrap(),
-            &[ScmpArgCompare::new(
-                2,
-                ScmpCompareOp::MaskedEqual(O_ACCMODE as u64),
-                O_RDONLY as u64,
-            )],
-        )
-        .unwrap();
-    context
-        .add_rule(ScmpAction::Allow, ScmpSyscall::from_name("read").unwrap())
         .unwrap();
 
     context.load().expect("apply seccomp filter");
 
     unsafe {
         image_classifier(
-            CStr::as_ptr(&CString::new(Path::new(filename).to_str().unwrap()).unwrap()),
+            CStr::as_ptr(
+                &CString::new(Path::new("data").join(filename).to_str().unwrap()).unwrap(),
+            ),
             top_k,
         )
     };
+
+    fs::remove_dir_all("/data").unwrap();
 }
